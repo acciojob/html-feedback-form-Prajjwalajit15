54efb48a4abc8297ea442e4977e635fd1f74963b
Prajjwalajit15 <122018880+Prajjwalajit15@users.noreply.github.com>
2023-08-12 21:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your code here
+it("should validate form elements", () => {
+  cy.visit(baseUrl + "/main.html");
+  cy.contains("Feedback Form");
+  
+  cy.get("form").within(() => {
+    cy.get("input#name")
+      .should("have.attr", "minlength", "3")
+      .should("have.attr", "maxlength", "50");
+      
+    cy.get("label[for='name']");
+    
+    cy.get("input#portfolio[type='url']")
+      .should("be.required");
+      
+    cy.get("label[for='portfolio']");
+    
+    cy.get("input#favColor[type='color']");
+    cy.get("label[for='favColor']");
+    
+    cy.get("input#likeness[type='range']")
+      .should("have.attr", "min", "1")
+      .should("have.attr", "max", "100");
+      
+    cy.get("label[for='likeness']");
+  });
+});
+
